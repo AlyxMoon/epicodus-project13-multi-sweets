5476728928cf063782b125348b860fdd9f08c4e5
AlyxMoon <allisterkmoon@gmail.com>
2021-08-15 09:45:38 -0700

    add frontend views for flavors

--- Marketplace/Models/Database/DatabaseContext.cs
+++ Marketplace/Models/Database/DatabaseContext.cs
@@ -1,9 +1,14 @@
 using Microsoft.EntityFrameworkCore;
 
+using Marketplace.Models;
+
 namespace Marketplace.Models.Database
 {
   public class DatabaseContext : DbContext
   {
+    public DbSet<Flavor> Flavors { get; set; }
+    public DbSet<Treat> Treats { get; set; }
+
     public DatabaseContext(DbContextOptions options) : base(options) { }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
